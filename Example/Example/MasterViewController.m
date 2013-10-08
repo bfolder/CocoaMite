53a1cc7a3ace47efa532a76e4e17c1e4f73b17a8
Heiko Dreyer <mail@boxedfolder.com>
2013-10-08 21:36:10 +0200

    Comments update

--- Example/Example/MasterViewController.m
+++ Example/Example/MasterViewController.m
@@ -2,7 +2,7 @@
 //  MasterViewController.m
 //  Example
 //
-//  Created by Heiko Dreyer on 30.09.13.
+//  Created by Heiko Dreyer on 09/30/13.
 //  Copyright (c) 2013 boxedfolder.com. All rights reserved.
 //
 
